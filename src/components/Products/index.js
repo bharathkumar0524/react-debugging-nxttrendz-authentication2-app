b89e8473d7e0736380a5a2f53e05139413e7c337
Bharath Kumar <bharathkumar.gajula22@gmail.com>
2021-09-02 14:40:10 +0000

    deugging-nxttrendz-authentication2

---
+++ src/components/Products/index.js
@@ -0,0 +1,26 @@
+import Cookies from 'js-cookie'
+
+import Header from '../Header'
+import './index.css'
+
+const Products = props => {
+  const {history} = props
+  const accessToken = Cookies.get('jwt_token')
+  if (accessToken === undefined) {
+    history.replace('/login')
+  }
+  return (
+    <>
+      <Header />
+      <div className="products-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+          alt="products"
+          className="products-img"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Products
